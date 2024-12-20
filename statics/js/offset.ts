bd270bc26ed178285bd39600ffb5965ad78a0a2e
HEX9CF <hex9cf@aliyun.com>
2024-12-20 22:44:49 +0800

    Update time offset

--- statics/js/offset.ts
+++ statics/js/offset.ts
@@ -1,12 +1,21 @@
-let offsetInput = document.getElementById('offset') as HTMLInputElement;
+let offsetSInput = document.getElementById('offset-s') as HTMLInputElement;
+let offsetMSInput = document.getElementById('offset-ms') as HTMLInputElement;
+let offsetBtn = document.getElementById('offset-btn') as HTMLButtonElement;
 
 let offset = 0;
 
-offsetInput.onchange = () => {
-    if (offsetInput.value === '') {
-        offset = 0;
+offsetBtn.onclick = () => {
+    let seconds = parseInt(offsetSInput.value);
+    if (isNaN(seconds)) {
+        seconds = 0;
+        offsetSInput.value = "0";
     }
-    offset = parseInt(offsetInput.value);
+    let milliseconds = parseInt(offsetMSInput.value);
+    if (isNaN(milliseconds)) {
+        milliseconds = 0;
+        offsetMSInput.value = "0";
+    }
+    offset = seconds * 1000 + milliseconds;
     console.log("时间偏移", offset);
     loadTable(tableData, offset);
 }
