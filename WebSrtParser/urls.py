d32b165ae5d89188cd596f0a6440853da5e0d1c5
HEX9CF <hex9cf@aliyun.com>
2024-12-20 14:59:51 +0800

    Add index router

--- WebSrtParser/urls.py
+++ WebSrtParser/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from WebSrtParser import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.hello, name="hello"),
 ]
